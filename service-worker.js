4b0f8085455503d220c6bb58fa82fd5a1765485f
Chr1sG1thub <140241398+Chr1sG1thub@users.noreply.github.com>
2025-11-08 16:17:13 +0100

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('v1').then(cache =>
+      cache.addAll([
+       '/wordleanalyser/index.html',
+       '/wordleanalyser/words.json'
+       ])
+    )
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response =>
+      response || fetch(event.request)
+    )
+  );
+});
